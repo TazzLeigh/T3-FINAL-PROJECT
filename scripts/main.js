cef391fe19a35212b76f5debee1f2599d1de2064
TazzLeigh <125860291+TazzLeigh@users.noreply.github.com>
2023-08-09 19:04:23 +0200

    Progress Milestone 1

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+//Checkout//
+  //Remove Item Button
+  $(".btn1").click(function(){
+    $("#item1").remove();
+    })
+  $(".btn2").click(function(){
+    $("#item2").remove();
+      })
+  $(".btn3").click(function(){
+    $("#item3").remove();
+        })
